2480f7a36b19c14d095905dfa1fe2af1e404fac5
NotForProffitt <55216953+NotForProffitt@users.noreply.github.com>
2021-01-30 11:15:39 -0500

    added functionality
    added !help command, expanded list of works/phrases that trigger the counter

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ bot.login(token)
 var daysSince = 0
 
 fs.readFile('daysSince.txt', 'utf8', (err, data) => {
-    if (err) {
+    if(err) {
         console.error(err)
         return
     }
@@ -28,6 +28,7 @@ var cannibalismCounter = 0
 //not technically used
 let lastMentionedDate = new Date()
 const prefix = '!'
+var reactEmoji = client.emojis.find(emoji => emoji.name === "fork_and_knife") 
 
 //string array for !lecter command
 var quotes = ["",
@@ -35,59 +36,77 @@ var quotes = ["",
     "I never feel guilty eating anything.",
     "Discourtesy is unspeakably ugly to me.",
     "Without death, we'd be at a loss. It's the prospect of death that drives us to greatness.",
-    "I do wish we could chat longer, but I'm having an old friend for dinner."]
+    "I do wish we could chat longer, but I'm having an old friend for dinner."
+]
+
+var keywords = ["cannibalism",
+                "cannibal",
+                "eat people",
+                "eat flesh",
+                "soylent green",
+                "yummy flesh",
+                "eat the rich",
+                "vore"
+
+]
 
 var interval = setInterval(increment, 86400000)
-
 function increment() {
     daysSince += 1
     fs.writeFile('daysSince.txt', daysSince, err => {
-        if (err) {
+        if(err) {
             console.error(err)
             return
         }
-        console.log('incremented DaySince count successfully')
+        console.log('incremented DaysSince count successfully')
     })
 }
 
 bot.on('message', async (msg) => {
-    if (msg.content.toLowerCase().includes("cannibalism") && !msg.author.bot) {
-        cannibalismCounter += 1
-        fs.writeFile('cannibalismCounter.txt', cannibalismCounter, err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-            console.log('incremented cannibalismCounter count successfully')
-        })
+    for(i = 0; i < keywords.length; i++) {
+        if(msg.content.toLowerCase().includes(keywords[i]) && !msg.author.bot) {
+            cannibalismCounter += 1
+            msg.react(reactEmoji)
+            fs.writeFile('cannibalismCounter.txt', cannibalismCounter, err => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+                console.log('incremented cannibalismCounter count successfully')
+            })
 
-        daysSince = 0
-        fs.writeFile('daysSince.txt', daysSince, err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-            console.log('reset DaySince count successfully')
-        })
-        lastMentionedDate = Date()
-        console.log(cannibalismCounter)
-        return
+            daysSince = 0
+            fs.writeFile('daysSince.txt', daysSince, err => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+                console.log('reset DaySince count successfully')
+            })
+            lastMentionedDate = Date()
+            console.log(cannibalismCounter)
+            return
+        }
     }
 
-    if (!msg.content.startsWith(prefix)) {
+    if(!msg.content.startsWith(prefix)) {
         console.log('no prefix')
         return
     }
 
+    if(msg.content.toLowerCase.startsWith("!help")) {
+        msg.reply("\`\`\`!help: shows this menu.\n!counter: see how long it has been since cannibalism was mentioned in this server.\n!wordcount: see how many times cannibalism has been mentioned in this server.\n!lecter: get a quote from everybody's favorite cannibal!\`\`\`")
+    }
+
     //responds with the amount of days since cannibalism was last mentioned
-    if (msg.content.toLowerCase().startsWith("!counter")) {
+    if(msg.content.toLowerCase().startsWith("!counter")) {
         console.log('counter call')
         msg.reply(daysSince + " days since cannibalism was last mentioned in this server.")
 
     }
 
     //replies with the amount of times the word cannibalism has been said in the server
-    if (msg.content.toLowerCase().startsWith("!wordcount")) {
+    if(msg.content.toLowerCase().startsWith("!wordcount")) {
         console.log('word count')
         fs.readFile('cannibalismCounter.txt', 'utf8', (err, data) => {
             if (err) {
@@ -98,7 +117,8 @@ bot.on('message', async (msg) => {
             cannibalismCounter = Number(data)
             console.log('read cannibalismCounter file successfully')
         })
-        msg.reply("Cannibalism has been mentioned " + cannibalismCounter + " times in this server. Delicious!")
+        if(cannibalismCounter = 1) msg.reply("Cannibalism has been mentioned " + cannibalismCounter + " time in this server. Delicious!")
+        else msg.reply("Cannibalism has been mentioned " + cannibalismCounter + " times in this server. Delicious!")
     }
 
     //replies with a quote from Hannibal Lector chosen at random from an array of responses
