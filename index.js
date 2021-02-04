0bd427b68cff9b71a326c9cbbf6e04212a173718
NotForProffitt <notthomasproffitt@gmail.com>
2021-02-04 15:04:50 -0500

    commented code

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var config = require('./config.js');
 const bot = new Discord.Client();
 const fs = require('fs')
 
-//shhhhh
-const token = 'ODA0NzU1MTQwNDkzOTY3NDIw.YBQ8oQ.hZBaWObLoXanhsG6mHNuj6lHRGw'
-
 bot.on('ready', () => {
     console.log('bot is ready')
 })
@@ -16,6 +13,7 @@ bot.login(require('./config'))
 
 var daysSince = 0
 
+//reads in the value stored in the daysSince file to the var
 fs.readFile('daysSince.txt', 'utf8', (err, data) => {
     if(err) {
         console.error(err)
@@ -27,38 +25,39 @@ fs.readFile('daysSince.txt', 'utf8', (err, data) => {
 })
 
 var cannibalismCounter = 0
-//not technically used
-let lastMentionedDate = new Date()
+
 const prefix = '!'
 
 //string array for !lecter command
-var quotes = ["",
-    "When the fox hears the rabbit scream, he comes a-runnin', but not to help!",
-    "I never feel guilty eating anything.",
-    "Discourtesy is unspeakably ugly to me.",
-    "Without death, we'd be at a loss. It's the prospect of death that drives us to greatness.",
-    "I do wish we could chat longer, but I'm having an old friend for dinner."
+var quotes = [
+	"",
+   	"When the fox hears the rabbit scream, he comes a-runnin', but not to help!",
+    	"I never feel guilty eating anything.",
+    	"Discourtesy is unspeakably ugly to me.",
+    	"Without death, we'd be at a loss. It's the prospect of death that drives us to greatness.",
+    	"I do wish we could chat longer, but I'm having an old friend for dinner."
 ]
 
-var keywords = ["cannibalism",
-                "cannibal",
-                "eat people",
-		"eating people",
-		"eat him",
-		"eat her",
-		"eat them",
-		"eat you",
-                "eat flesh",
-		"eat me",
-                "soylent green",
-                "yummy flesh",
-                "eat the rich",
-                "vore",
-		"namira",
-		"canibalismo"
-
+var keywords = [
+	"cannibalism",
+        "cannibal",
+        "eat people",
+	"eating people",
+	"eat him",
+	"eat her",
+	"eat them",
+	"eat you",
+	"eat flesh",
+	"eat me",
+        "soylent green",
+        "yummy flesh",
+        "eat the rich",
+        "vore",
+	"namira",
+	"canibalismo"
 ]
 
+//counter for incrementing daysSince var every 24 hours
 var interval = setInterval(increment, 86400000)
 function increment() {
     daysSince++
@@ -73,8 +72,12 @@ function increment() {
 
 bot.on('message', async (msg) => {
     for(i = 0; i < keywords.length; i++) {
+	
+	//if msg contains reference, reset daysSince and increment cannibalismCounter
         if(msg.content.toLowerCase().includes(keywords[i]) && !msg.author.bot && !(msg.guild === null)) {
             cannibalismCounter++
+	    daysSince = 0
+
             fs.writeFile('cannibalismCounter.txt', cannibalismCounter, err => {
                 if (err) {
                     console.error(err)
@@ -83,7 +86,6 @@ bot.on('message', async (msg) => {
                 console.log('incremented cannibalismCounter count successfully')
             })
 
-            daysSince = 0
             fs.writeFile('daysSince.txt', daysSince, err => {
                 if (err) {
                     console.error(err)
@@ -97,11 +99,13 @@ bot.on('message', async (msg) => {
         }
     }
 
+    //this is here because I want it be, no other reason
     if(!msg.content.startsWith(prefix)) {
         console.log('no prefix')
         return
     }
 
+    //because every good bot has one
     if(msg.content.toLowerCase().startsWith("!help")) {
         msg.reply("\`\`\`!help: shows this menu.\n!counter: see how long it has been since cannibalism was mentioned in this server.\n!wordcount: see how many times cannibalism has been mentioned in this server.\n!lecter: get a quote from everybody's favorite cannibal!\n\nContact ProbablyNotAFurry#6782 for issues, questions, or comments.\`\`\`")
     }
@@ -123,10 +127,11 @@ bot.on('message', async (msg) => {
             }
             console.log(data)
             cannibalismCounter = Number(data)
+
+	    //a whole if/else block because bendy is a grammer stickler >:(
 	    if (cannibalismCounter == 1) {
 	    	msg.reply("Cannibalism has been mentioned 1 time in this server. Delicious!")
 	    }
-	
 	    else {
 	    msg.reply("Cannibalism has been mentioned " + cannibalismCounter + " times in this server. Delicious!")
 	    }
