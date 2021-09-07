7cccf82040635736c3c39f3104b1acad002c1d26
NotForProffitt <you@example.com>
2021-09-07 01:35:36 -0400

    test

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ bot.on("guildCreate", (guild) => {
     // This event triggers when the bot joins a guild.    
     console.log(`Joined new guild: ${guild.name}`)
     //sanitized SQL input
-    database.query("INSERT INTO guild (guildID, cannibalismCounter, daysSince, lastTime DEFAULT 'nothing here!') VALUES (?,1,0,?) ON DUPLICATE KEY UPDATE cannibalismCounter = cannibalismCounter + 1, daysSince = 0, lastTime = ?;",[
-        database.escape(guild.id.toString()),
+    database.query("INSERT INTO guild (guildID, cannibalismCounter, daysSince, lastTime) VALUES (?,1,0,?) ON DUPLICATE KEY UPDATE cannibalismCounter = cannibalismCounter + 1, daysSince = 0, lastTime = ?;",[
+        guild.id.toString(),
         database.escape(lastReference),
         database.escape(lastReference)
     ])
